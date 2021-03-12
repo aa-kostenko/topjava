e46ae8500d9c1ea659cc773b33eb79cddb792cdf
dev <dev@example.com>
2021-03-12 22:29:07 +0300

    Separated JdbcMealRepository for HSQLDB and Postgres via @Profile

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/HSQLDBJdbcMealRepository.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Repository
+@Profile("hsqldb")
+public class HSQLDBJdbcMealRepository extends JdbcMealRepository{
+    public HSQLDBJdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    protected Timestamp convertLocalDateTime(LocalDateTime dateTime) {
+        return Timestamp.valueOf(dateTime);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/PostgresJdbcMealRepository.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Repository
+@Profile("postgres")
+public class PostgresJdbcMealRepository extends JdbcMealRepository{
+    public PostgresJdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    protected LocalDateTime convertLocalDateTime(LocalDateTime dateTime) {
+        return dateTime;
+    }
+}
