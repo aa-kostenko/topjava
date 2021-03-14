fcd7452c95766fe70c821c6d2b1ffd83a6bb3632
dev <dev@example.com>
2021-03-14 11:50:10 +0300

    get user by id along with their food made and tested in services (tests and implementation - only for Data Jpa)

--- src/test/java/ru/javawebinar/topjava/service/UserServiceDataJpaTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceDataJpaTest.java
@@ -1,7 +1,24 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.User;
+
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_MATCHER;
 
 @ActiveProfiles("data-jpa")
 public class UserServiceDataJpaTest extends AbstractUserServiceTest{
+    @Autowired
+    private UserService service;
+
+    @Test
+    public void getWithMeals() {
+        User user = service.getWithMeals(USER_ID);
+        Assert.assertEquals(user.getMeals().size(), MealTestData.meals.size());
+    }
 }
