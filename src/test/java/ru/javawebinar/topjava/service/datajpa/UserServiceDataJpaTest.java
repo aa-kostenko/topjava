6e246d6c7b3ec3289da7637757b84a8736bb85f4
dev <dev@example.com>
2021-03-23 22:58:05 +0300

    UserServiceDataJpaTest, test case when user have not meals

--- src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJpaTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 import ru.javawebinar.topjava.service.UserService;
@@ -21,5 +22,14 @@ public class UserServiceDataJpaTest extends AbstractUserServiceTest {
     public void getWithMeals() {
         User user = service.getWithMeals(USER_ID);
         Assert.assertEquals(user.getMeals().size(), MealTestData.meals.size());
+    }
+
+    @Test
+    public void getWithMealsUserNoHaveMeals() {
+        User newUser =  UserTestData.getNew();
+        service.create(newUser);
+        int newUserId = newUser.getId();
+        User user = service.getWithMeals(newUserId);
+        Assert.assertEquals(user.getMeals().size(), 0);
     }
 }
